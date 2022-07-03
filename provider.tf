af0fe58dccde3c24dac86f1b27117c483a576dba
tany78 <90527319+tany78@users.noreply.github.com>
2022-07-03 23:16:08 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,6 +0,0 @@
-provider "vsphere" {
-  vsphere_server        = var.vsphere_vcenter
-  user                  = var.vsphere_user
-  password              = var.vsphere_password
-  allow_unverified_ssl  = true
-}
